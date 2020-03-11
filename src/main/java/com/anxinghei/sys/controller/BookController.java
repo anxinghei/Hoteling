105c87b47e992b75a03bd4b2a2372ed2101db112
anxinghei <846939029@qq.com>
2020-03-11 15:45:53 +0800

    跨域请求

--- src/main/java/com/anxinghei/sys/controller/BookController.java
+++ src/main/java/com/anxinghei/sys/controller/BookController.java
@@ -3,7 +3,17 @@ package com.anxinghei.sys.controller;
 //import com.github.wxiaoqi.security.common.rest.BaseController;
 //import com.anxinghei.sys.biz.BookBiz;
 import com.anxinghei.sys.entity.Book;
+import com.anxinghei.sys.mapper.BookMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("book")
 public class BookController  {
+	
+	@Autowired
+	private BookMapper bookMapper;
+	
+	@GetMapping("findAll/{start}/{size}")
+	PageInfo<Book> findAll(@PathVariable("start") Integer start,@PathVariable("size") Integer size){
+		PageHelper.startPage(start,size);
+		List<Book> books=bookMapper.selectAll();
+		PageInfo<Book> pageInfo=new PageInfo<Book>(books);
+		return pageInfo;
+	}
 
 }
--- src/main/java/com/anxinghei/sys/entity/Type.java
+++ src/main/java/com/anxinghei/sys/entity/Type.java
@@ -4,34 +4,36 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.*;
 
-
 /**
  * 房间类别表
  * 
  * @author Autumn
- * @email 
+ * @email
  * @date 2020-03-10 20:31:52
  */
 @Table(name = "type")
 public class Type implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	    //
-    @Id
-    private Integer id;
-	
-	    //可住人数
-    @Column(name = "lives")
-    private Integer lives;
-	
-	    //价格
-    @Column(name = "price")
-    private Integer price;
-	
-	    //房间大小
-    @Column(name = "area")
-    private Integer area;
-	
+
+	//
+	@Id
+	private Integer id;
+
+	// 可住人数
+	@Column(name = "lives")
+	private Integer lives;
+
+	// 房间类别名
+	@Column(name = "name")
+	private Integer name;
+
+	// 价格
+	@Column(name = "price")
+	private Integer price;
+
+	// 房间大小
+	@Column(name = "area")
+	private Integer area;
 
 	/**
 	 * 设置：
@@ -39,11 +41,19 @@ public class Type implements Serializable {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	/**
 	 * 获取：
 	 */
 	public Integer getId() {
 		return id;
+	}
+	public Integer getName() {
+		return name;
+	}
+
+	public void setName(Integer name) {
+		this.name = name;
 	}
 	/**
 	 * 设置：可住人数
@@ -51,30 +61,35 @@ public class Type implements Serializable {
 	public void setLives(Integer lives) {
 		this.lives = lives;
 	}
+
 	/**
 	 * 获取：可住人数
 	 */
 	public Integer getLives() {
 		return lives;
 	}
+
 	/**
 	 * 设置：价格
 	 */
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
+
 	/**
 	 * 获取：价格
 	 */
 	public Integer getPrice() {
 		return price;
 	}
+
 	/**
 	 * 设置：房间大小
 	 */
 	public void setArea(Integer area) {
 		this.area = area;
 	}
+
 	/**
 	 * 获取：房间大小
 	 */
