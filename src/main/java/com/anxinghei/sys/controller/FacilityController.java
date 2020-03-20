c2cbed811db03e7b0ace9b80f3b28e792a2b08dc
anxinghei <846939029@qq.com>
2020-03-20 22:29:59 +0800

    设施的增删

--- src/main/java/com/anxinghei/sys/controller/FacilityController.java
+++ src/main/java/com/anxinghei/sys/controller/FacilityController.java
@@ -1,13 +1,53 @@
 package com.anxinghei.sys.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// extends BaseController<FacilityBiz,Facility>
+import com.anxinghei.sys.entity.Facility;
+import com.anxinghei.sys.mapper.FacilityMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 
 @RestController
 @RequestMapping("facility")
 public class FacilityController  {
 	
+	@Autowired
+	private FacilityMapper facilityMapper;
+	
+	@GetMapping("findAll/{start}/{size}")
+	PageInfo<Facility> findAll(@PathVariable("start") Integer start,@PathVariable("size") Integer size){
+		PageHelper.startPage(start,size);
+		List<Facility> facilitys=facilityMapper.selectAll();
+		PageInfo<Facility> pageInfo=new PageInfo<Facility>(facilitys);	
+		return pageInfo;
+	}
+	
+	@DeleteMapping("deleteById/{id}")
+	public String deleteById(@PathVariable("id")Integer id) {
+		int isDel=facilityMapper.deleteByPrimaryKey(id);
+		if (isDel==1) {
+			return "success";
+		}
+		return "error";
+	}
+	
+	@PostMapping("save")
+	public String save(@RequestBody Facility facility) {
+		int isAdded=facilityMapper.insert(facility);
+		if (isAdded==1) {
+			return "success";
+		}
+		return "error";
+	}
 
 }
